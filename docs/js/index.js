4dc5349f37681bde658d9379a61d891b6ac9b970
Balraj <polltery@gmail.com>
2017-02-13 02:45:17 +0400

    Update project to angular framework

---
+++ docs/js/index.js
@@ -0,0 +1,25 @@
+/*
+Author(s) : Balraj Singh Bains
+Date : 12/01/2017
+*/
+
+var app = angular.module('network-application', ['ngMaterial','ngRoute']);
+
+app.config(function($routeProvider) {
+
+    $routeProvider
+    .when("/", {
+        templateUrl : "views/overview.html"
+    })
+    
+    .when("/login", {
+        templateUrl : "views/login.html"
+    });
+
+});
+
+app.controller('overview-controller', function($scope, $location){
+    $scope.changeView = function(view){
+        $location.path(view);
+    };
+});
