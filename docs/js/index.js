7c1103ba3efd20bf324757948658f0c078b36851
Balraj <polltery@gmail.com>
2017-02-13 20:07:49 +0400

    Add index controller and more
    More:
    - Add profile controller
    - Add profile view
    - Update all changeView methods in controllers
    - Minor changes to login view

--- docs/js/index.js
+++ docs/js/index.js
@@ -14,18 +14,58 @@ app.config(function($routeProvider) {
     
     .when("/login", {
         templateUrl : "views/login.html"
+    })
+    
+    .when("/profile", {
+        templateUrl : "views/profile.html"
     });
 
 });
 
-app.controller('overview-controller', function($scope, $location){
+app.controller('index-controller', function($rootScope, $scope, $location){
+    
+    $rootScope.isOverview = true;
     $scope.changeView = function(view){
+        if(view === '/'){
+            $rootScope.isOverview = true;
+        }else{
+            $rootScope.isOverview = false;
+        }
+        $location.path(view);
+    };
+
+});
+
+app.controller('overview-controller', function($scope, $location, $rootScope){
+    $scope.changeView = function(view){
+        if(view === '/'){
+            $rootScope.isOverview = true;
+        }else{
+            $rootScope.isOverview = false;
+        }
         $location.path(view);
     };
 });
 
-app.controller('login-controller', function($scope, $location){
+app.controller('login-controller', function($scope, $location, $rootScope){
     $scope.changeView = function(view){
-        $location.path(view)
+        if(view === '/'){
+            $rootScope.isOverview = true;
+        }else{
+            $rootScope.isOverview = false;
+        }
+        $location.path(view);
+    };
+});
+
+app.controller('profile-controller', function($scope){
+    $scope.username = "username";
+        $scope.changeView = function(view){
+        if(view === '/'){
+            $rootScope.isOverview = true;
+        }else{
+            $rootScope.isOverview = false;
+        }
+        $location.path(view);
     };
 });
