0733f1747d74aeeb5ac521561c1498bc5b204f39
Balraj <polltery@gmail.com>
2017-03-18 01:15:22 +0400

    Add instagram client details for services

---
+++ docs/js/env.js
@@ -0,0 +1,10 @@
+// Enviorment variables
+window.ENV = {
+    // Instagram variables
+    instagramClient : {
+        clientId : "24fdee85a464408caea2ec1ab8f5d42f",
+        redirectUri : "http://localhost:8080",
+        responseType : "token",
+        scope : "basic+public_content"
+    }
+}
