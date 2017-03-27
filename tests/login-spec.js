51188b222249e972da23591109b3b58834a826b9
Balraj <polltery@gmail.com>
2017-03-27 15:33:07 +0400

    Add login success scenario to spec

--- tests/login-spec.js
+++ tests/login-spec.js
@@ -6,10 +6,10 @@
 // First testset description
 describe('Middleware login', function(){
 
+var EC = protractor.ExpectedConditions;
+
   // description of this test
   it('Should give error when non-user logs in', function(){
-
-    var EC = protractor.ExpectedConditions;
 
     // Browser gets the given url
     browser.get('https://polltery.github.io/middleware-frontend/index.html#/login');
@@ -42,6 +42,30 @@ describe('Middleware login', function(){
         expect(statusId.getText()).toContain("User not found");
       }
     });
+  });
+
+  //Description of this test
+  it('Should log in a registered user', function(){
+
+    // Browser gets the given url
+    browser.get('https://polltery.github.io/middleware-frontend/index.html#/login');
+
+    // Set the form variables (example, example) is already set
+    element(by.model('loginUsername')).sendKeys('example');
+    element(by.model('loginPassword')).sendKeys('example');
+
+    // Waiting for the button to be clickable solution from http://stackoverflow.com/questions/36871071/click-function-isnt-working-in-protractor-scripts
+    // Get the login button in a variable
+    var loginButton = element(by.id('login-submit'));
+
+    // Wait for it to be clickable
+    browser.wait(EC.elementToBeClickable(loginButton), 5000);
+
+    // Click the login button that was saved before
+    loginButton.click();
+
+    // Expect the browser to go to user profile
+    expect(browser.getCurrentUrl()).toEqual('https://polltery.github.io/middleware-frontend/index.html#/profile/example'); 
 
   });
 });
