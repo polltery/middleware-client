602ae14bd3690fadf0cda1d3b88d26c77ca3236c
Balraj <polltery@gmail.com>
2017-03-29 16:02:35 +0400

    Add API variables

--- docs/js/env.js
+++ docs/js/env.js
@@ -1,5 +1,6 @@
 // Enviorment variables
 window.ENV = {
+    apiUrl : "http://localhost:8082/socialhub/api",
     // Instagram variables
     instagramClient : {
         clientId : "24fdee85a464408caea2ec1ab8f5d42f",
