e5386b133b6a21c783f79e5a9eab958bfe18ca5e
Balraj <polltery@gmail.com>
2017-04-06 16:09:41 +0400

    Fix Instagram redirect URI

--- docs/js/env.js
+++ docs/js/env.js
@@ -4,7 +4,7 @@ window.ENV = {
     // Instagram variables
     instagramClient : {
         clientId : "24fdee85a464408caea2ec1ab8f5d42f",
-        redirectUri : "http://localhost:8080/docs/index.html",
+        redirectUri : "http://localhost:8080/index.html",
         responseType : "token",
         scope : "basic+public_content"
     }
